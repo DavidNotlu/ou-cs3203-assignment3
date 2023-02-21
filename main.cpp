e0278f530f27996ba727e41e823e4b64b92e278a
David Lu <david07.lu@gmail.com>
2023-02-21 16:53:55 -0600

    add product function

--- main.cpp
+++ main.cpp
@@ -3,12 +3,19 @@
 #include <vector>
 using namespace std;
 class program {
-    
+    // Adds all values in list
     int sum(vector<int> list) {
         int sum = 0;
         for(vector<int>::iterator it = list.begin(); it != list.end(); ++it)
              sum += *it;
         return sum;
+    }
+    // Mulitplies all values in list
+    int product(vector<int> list) {
+        int product = 0;
+        for(vector<int>::iterator it = list.begin(); it != list.end(); ++it)
+             product *= *it;
+        return product;
     }
 
 
